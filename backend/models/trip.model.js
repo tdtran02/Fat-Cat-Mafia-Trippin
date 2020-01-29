b803fccbbcc1fde2a2f0df764c43f5515d8ac993
Dongxuan Li <davidli@dhcp-39-46-171.dhcpint.csulb.edu>
2020-01-29 09:23:01 -0800

    Create trip.model.js

---
+++ backend/models/trip.model.js
@@ -0,0 +1,11 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// let Todo = new Schema({
+//     user: {
+//         List
+//     },
+//    date
+// });
+
+// module.exports = mongoose.model('Todo', Todo);
