1dbd9edce51080c0c889bc263904185db2b69742
Dongxuan Li <davidli@dhcp-39-46-171.dhcpint.csulb.edu>
2020-01-29 10:33:17 -0800

    added routes

--- backend/models/trip.model.js
+++ backend/models/trip.model.js
@@ -1,11 +1,1 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
 
-// let Todo = new Schema({
-//     user: {
-//         List
-//     },
-//    date
-// });
-
-// module.exports = mongoose.model('Todo', Todo);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Register from "./components/Register";
+import Signin from "./components/Signin";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* home page */}
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/signin">Sign in</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/signin">
+              <Signin />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
