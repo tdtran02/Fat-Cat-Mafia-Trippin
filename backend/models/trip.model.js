0e5863de70e6f205b3f41ce42f43c2726cf7605e
tdtran02 <32653978+tdtran02@users.noreply.github.com>
2020-02-03 00:02:00 -0800

    create schema and model for trip and user

--- backend/models/trip.model.js
+++ backend/models/trip.model.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+//Creating Schema and Model for trip
+const tripSchema = new Schema({
+    destination: String,
+    date: Date,
+    length: Number
+});
+
+const TripSchem = mongoose.model('tripchar', tripSchema);
+
+module.exports = TripSchem;
+
+var user = new TripSchem({})
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,1 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+//Creating Schema and Model for user
+const userSchema = new Schema({
+    first_name: String,
+    last_name: String,
+    user_id: String,
+    email: String,
+    password: String
+});
+
+const UserChar = mongoose.model('userchar', userSchema);
+
+module.exports = UserChar;
+
+var user = new UserChar({})
