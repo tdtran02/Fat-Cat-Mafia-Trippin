081e724c323ed8d76e1b6d0f29669954e04d2f6c
tdtran02 <32653978+tdtran02@users.noreply.github.com>
2020-02-05 09:39:11 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Register from "./components/Register";
 import Signin from "./components/Signin";
-import Email from "./components/ValidateEmail";
 
 function App() {
   return (
@@ -35,10 +34,6 @@ function App() {
 
             <Route path="/signin">
               <Signin />
-            </Route>
-
-            <Route path="/email">
-              <Email />
             </Route>
 
           </Switch>
