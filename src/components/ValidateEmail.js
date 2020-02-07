8fb193656a99f334023bc4961b77cf65ec669ce9
tdtran02 <32653978+tdtran02@users.noreply.github.com>
2020-02-07 14:25:32 -0800

    working on validate email

--- src/components/ValidateEmail.js
+++ src/components/ValidateEmail.js
@@ -1,31 +1,27 @@
 import React, { Component } from "react";
 
 export class ValidateEmail extends Component {
+    ValEmail(e){
+        let getEmail = document.getElementById("email");
+        console.log("Email: " + getEmail);
+    }
+
   render() {
  //   var regexConst = new RegExp('^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z]*@[0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$')
     return (
-        
-        
-        <html>
-        <head> 
-            <title>Text Box</title>
-        </head>
-            <body>
+        <div>
                 <form action = " ">
                     <p>
                         <label>Enter email: 
                             <input type="email" myemail="myemail" id="email"></input>
                         </label>                
 
-                        <button id="button">Validate Email</button>
+                        <button id="button" onClick={this.ValEmail}>Validate Email</button>
 
-                        
-                
                 </p>
                 </form>
-
-            </body>
-        </html>
+        </div>
+        
       
     
         
