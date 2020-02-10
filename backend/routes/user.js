99365cdf3d3f98f5686b0fdb7b5a8b91e8f07e07
Dongxuan Li <davidli@dhcp-39-5-83.dhcpint.csulb.edu>
2020-02-10 10:09:43 -0800

    User put api

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -17,4 +17,19 @@ USERROUTES.route("/user/:id").get(function(req, res) {
   });
 });
 
+USERROUTES.route("/user/:id").put(function(req, res) {
+  console.log(req.body.hi);
+  console.log(req.params);
+
+  USER.updateOne(
+    { _id: req.params.id },
+    { $set: { first_name: req.body.picture } }
+  )
+    .then(response => {
+      res.status(200).json({});
+      console.log(response);
+    })
+    .catch(err => {});
+});
+
 module.exports = USERROUTES;
