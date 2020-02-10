4b24898f866837bc89c2c0b5100f6084eab00998
tdtran02 <32653978+tdtran02@users.noreply.github.com>
2020-02-10 10:21:42 -0800

    Update ValidateEmail.js

--- src/components/ValidateEmail.js
+++ src/components/ValidateEmail.js
@@ -3,29 +3,62 @@ import React, { Component } from "react";
 export class ValidateEmail extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+          email: '',
+          pattern: /^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z]*@[0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/,
+          password: ''
+        };
     
+        this.handleEmail = this.handleEmail.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+      }
+    
+      handleEmail(event) {
+        const value = event.target.value;
+        this.setState({email: value});
+      }
+
+      handlePassword(event) {
+        const value = event.target.value;
+        this.setState({password: value});
       }
     
       handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert(this.state.value + ' is valid.');
+        if (this.state.pattern.test(this.state.email)){
+          this.sendEmail();
+        }
+        else{
+          alert("Please enter correct email format.");
+        }
+        if (this.state.password !== null){
+          alert("Password: " + this.state.password);
+        }
+        else{
+          alert("Enter password.");
+        }
         event.preventDefault();
       }
+
+      sendEmail(event) {
+        alert(this.state.email + ' is valid');
+
+      }
+
     
       render() {
         return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Enter email:
-              <input type="email" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Validate" />
+          <form onSubmit={this.handleChange}>
+            <div>
+              <label>
+                <input type="email" placeholder="Email *" value={this.state.email} onChange={this.handleEmail}/>
+              </label>
+              <label>
+                <input type="password" placeholder="Password *" value={this.state.password} onChange={this.handlePassword}/>
+              </label>
+                <input type="submit" value="Validate" onChange={this.handleChange}/>
+            </div>
+            
           </form>
         );
       }
