54fbed353e66cf28ffc314a9b5894e12a266c6f4
Dongxuan Li <davidli@Davids-MacBook-Pro.local>
2020-02-15 13:19:21 -0800

    Added to navbar page

--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -9,120 +9,129 @@ import Email from "./ValidateEmail";
 import Home from "./Home";
 import MyAccount from "./MyAccount";
 import Trip from "./Trip";
+import Friends from "./Friends";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    Container
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Container
 } from "reactstrap";
 
 class AppNavBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOpen: false,
-            user: null
-        };
-    }
-
-    componentDidMount() {
-        this.setState({ user: JSON.parse(localStorage.getItem("user")) });
-    }
-
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      user: null
     };
+  }
 
-    logout = () => {
-        localStorage.removeItem("user");
-        document.location.href = "/";
-    };
+  componentDidMount() {
+    this.setState({ user: JSON.parse(localStorage.getItem("user")) });
+  }
 
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
 
-    render() {
-        const LOGGED_IN = (
-            <Fragment>
-                <NavItem>
-                    <NavLink tag={RRNavLink} exact to="/home">
-                        Home
-                    </NavLink>
-                </NavItem>
-                <NavItem style={{ cursor: "pointer" }}>
-                    <NavLink>Logged in!</NavLink>
-                </NavItem>
-                <NavItem style={{ cursor: "pointer" }} onClick={this.logout}>
-                    <NavLink>Log out</NavLink>
-                </NavItem>
-                <NavItem style={{ cursor: "pointer" }}>
-                    <NavLink tag={RRNavLink} exact to="/Trip">Trip</NavLink>
-                </NavItem>
-            </Fragment>
-        );
+  logout = () => {
+    localStorage.removeItem("user");
+    document.location.href = "/";
+  };
 
-        const LOGGED_OUT = (
-            <Fragment>
-                <NavItem>
-                    <NavLink tag={RRNavLink} exact to="/Login">
-                        Log In
+  render() {
+    const LOGGED_IN = (
+      <Fragment>
+        <NavItem>
+          <NavLink tag={RRNavLink} exact to="/home">
+            Home
           </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink tag={RRNavLink} exact to="/Register">
-                        Register
+        </NavItem>
+        <NavItem>
+          <NavLink tag={RRNavLink} exact to="/friends">
+            Friends
           </NavLink>
-                </NavItem>
-            </Fragment>
-        );
-        return (
-            <div style={{ height: "100px !important" }}>
-                <Router>
-                    <Navbar color="dark" dark expand="sm" className="mb-5">
-                        <Container>
-                            <NavbarBrand href="/">Trippin</NavbarBrand>
-                            <NavbarToggler onClick={this.toggle} />
-                            <Collapse isOpen={this.state.isOpen} navbar>
-                                <Nav className="ml-auto" navbar>
+        </NavItem>
+        <NavItem style={{ cursor: "pointer" }}>
+          <NavLink>Logged in!</NavLink>
+        </NavItem>
+        <NavItem style={{ cursor: "pointer" }} onClick={this.logout}>
+          <NavLink>Log out</NavLink>
+        </NavItem>
+        <NavItem style={{ cursor: "pointer" }}>
+          <NavLink tag={RRNavLink} exact to="/Trip">
+            Trip
+          </NavLink>
+        </NavItem>
+      </Fragment>
+    );
 
-                                    <NavItem>
-                                        <NavLink tag={RRNavLink} exact to="/email">
-                                            Email
+    const LOGGED_OUT = (
+      <Fragment>
+        <NavItem>
+          <NavLink tag={RRNavLink} exact to="/Login">
+            Log In
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={RRNavLink} exact to="/Register">
+            Register
+          </NavLink>
+        </NavItem>
+      </Fragment>
+    );
+    return (
+      <div style={{ height: "100px !important" }}>
+        <Router>
+          <Navbar color="dark" dark expand="sm" className="mb-5">
+            <Container>
+              <NavbarBrand href="/">Trippin</NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink tag={RRNavLink} exact to="/email">
+                      Email
                     </NavLink>
-                                    </NavItem>
-                                    {this.state.user ? LOGGED_IN : LOGGED_OUT}
-                                </Nav>
-                            </Collapse>
-                        </Container>
-                    </Navbar>
-                    <Switch>
-                        <Route path="/trip">
-                            <Trip />
-                        </Route>
-                        <Route path="/register">
-                            <Register />
-                        </Route>
-                        <Route path="/login">
-                            <Login />
-                        </Route>
-                        <Route path="/myAccount">
-                            <MyAccount />
-                        </Route>
-                        <Route path="/email">
-                            <Email />
-                        </Route>
+                  </NavItem>
+                  {this.state.user ? LOGGED_IN : LOGGED_OUT}
+                </Nav>
+              </Collapse>
+            </Container>
+          </Navbar>
+          <Switch>
+            <Route path="/trip">
+              <Trip />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/myAccount">
+              <MyAccount />
+            </Route>
+            <Route path="/email">
+              <Email />
+            </Route>
+            <Route path="/friends">
+              <Friends />
+            </Route>
 
-                        <Route path="/home">
-                            <Home />
-                        </Route>
-                    </Switch>
-                </Router>
-            </div>
-        );
-    }
+            <Route path="/home">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default AppNavBar;
