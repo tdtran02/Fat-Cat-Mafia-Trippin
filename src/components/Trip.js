b8727e8d0bb08b2b5eda1b83292a64d650044b80
beibeijoy <xinbeishen@gmail.com>
2020-02-16 19:19:20 -0800

    Display Trip Page

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -10,47 +10,62 @@ export class Trip extends Component {
         this.state = { editPhotoShow: false, option: '2' };
 
     }
-
-
-
     render() {
-
-
         return (
-            <div className="container-form">
-                <div className="panel panel-primary">
+            <div className="background-container">
+                {/*<div>
+                    <img className="pic-background"
+                    src={require("./images/trip_photo_2.png")}
+                    alt="road"
+                    />
+                </div>*/}
+                {/*<div className="container-form">
                     <div className="panel-heading">
-                        <h3 className="panel-title">TRAVEL</h3>
-                    </div>
-                    <div className="panel-body">
-                        <br></br>
-                        <div id="location">
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <label className="control-label">Departure</label>
-                                    <input className="form-control" id="departue_location" placeholder="City Country Depart"></input>
+                            <h3 className="panel-title">Start Your New Itinerary </h3>
+                        </div>
+                </div>*/}
+                <div className="container-form">
+                    <div className="panel panel-primary">
+                        <div className="panel-heading">
+                            <h3 className="panel-title">Itinerary</h3>
+                        </div>
+                        <div className="panel-body">
+                            <br></br>
+                            <div id="location">
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <label className="control-label">Departure</label>
+                                        <input className="form-control" id="departue_location" placeholder="City Country Depart"></input>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <label className="control-label" color="white" >Arrival</label>
+                                        <input className="form-control" id="arrival_location" placeholder="City Country Arrive"></input>
+                                    </div>
                                 </div>
-                                <div className="col-md-6">
-                                    <label className="control-label">Arrival</label>
-                                    <input className="form-control" id="arrival_location" placeholder="City Country Arrive"></input>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <form>
+                                            <label htmlFor="control-label">Start Date
+                                                <input className="form-control" type="date" name="start-day" min="2020-02-10" max="2022-02-10" required></input>
+                                                <span className="validity"></span>
+                                            </label>
+                                        </form>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <form>
+                                            <label htmlFor="control-label">End Date
+                                                <input className="form-control" type="date" name="end-day" min="2020-02-11" max="2022-02-11" required></input>
+                                                <span className="validity"></span>
+                                            </label>
+                                        </form>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <form>
-                                        <label htmlFor="control-label">Start Date
-                        <input className="form-control" type="date" name="start-day" min="2020-02-10" max="2022-02-10" required></input>
-                                            <span className="validity"></span>
-                                        </label>
-                                    </form>
-                                </div>
-                                <div className="col-md-6">
-                                    <form>
-                                        <label htmlFor="control-label">End Date
-                        <input className="form-control" type="date" name="end-day" min="2020-02-11" max="2022-02-11" required></input>
-                                            <span className="validity"></span>
-                                        </label>
-                                    </form>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <Button className="ml-3" variant="info" onClick={this.onSearchFieldClick}>
+                                        Create
+                                        </Button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
