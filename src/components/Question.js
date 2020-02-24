fb910f235d2352eece8443b2a4b399e95bbfbfa7
tdtran02 <32653978+tdtran02@users.noreply.github.com>
2020-02-24 11:00:15 -0800

    trip

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,98 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {QuestionData} from './QuestionData';
 
-export class Question extends Component {
-  state = {
-    answer: null,
-    currentQuestion: 0,
-    options: [],
-    quizEnd: false
-  }
+class Question extends Component {
 
-    
-      loadSurvey = () => {
-        const {currentQuestion} = this.state;
-        this.setState(() => {
-          return{
-            questions: QuestionData[currentQuestion].question,
-            options: QuestionData[currentQuestion].options,
-            answers: QuestionData[currentQuestion].answer
-          }
-        })
-      }
-      
-      componentDidMount() {
-        this.loadSurvey();
-      }
-
-      nextQuestion = () => {
-        this.setState({
-          currentQuestion: this.state.currentQuestion + 1
-        })
-        
-      }
-
-      componentDidUpdate(prevProps, prevState){
-        const {currentQuestion} = this.state;
-        if(this.state.currentQuestion !== prevState.currentQuestion){
-          this.setState(() => {
-            return{
-              questions: QuestionData[currentQuestion].question,
-              options: QuestionData[currentQuestion].options,
-              answers: QuestionData[currentQuestion].answer
-            }
-          })
-        }
-      }
-
-      setAnswer = answer =>{
-        this.setState({
-          userAnswer: answer
-        })
-      }
-
-      setFinish = () =>{
-        if(this.state.currentQuestion === QuestionData.length - 1){
-          this.setState({
-            quizEnd: true
-          })
-        }
-      }
-      
-
-      render(){
-        const {questions, options, currentQuestion, userAnswer} = this.state;
-        
-        return (
-          <div className="App">
-            <h3>{questions}</h3>
-            <span>{'Questions'} ${currentQuestion + 1} {' out of'} ${QuestionData.length + 1} </span>
-              {options.map(option => (
-                <p
-                  key = {option.id}
-                  className={'ui floating message options ${userAnswer === option ? "selected" : null'} 
-                  onClick={() => this.setAnswer(option)}
-                > 
-                  {option} 
-                </p>
-              ))}
-              {currentQuestion < QuestionData.length - 1 &&
-                <button 
-                  onClick = {this.nextQuestion}
-                >Next</button>
-              }
-              {currentQuestion === QuestionData.length - 1 &&
-                <button
-                  onClick={this.setFinish}
-                >Finish</button>
-              }
-          </div>
-        )
-      }
-  }
-  
-  /* working block
   handleSubmit(event) {
     this.setState({ answer: event.target.value });
   }
@@ -110,7 +20,6 @@ export class Question extends Component {
     )
   }
 }
-*/
 /*
 function Question(props) {
   return (
--- src/components/Survey.js
+++ src/components/Survey.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import { Alert } from "react-bootstrap";
-import {QuestionData} from './QuestionData';
 
 const AXIOS = require("axios").default;
 export class Survey extends Component{
@@ -13,34 +12,63 @@ export class Survey extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            userAnswer: null,
-            currentQuestion: 0,
-            options: [],
-            quizEnd: false
-          };
-    };
+            questions: [
+                {
+                    questionId: 1,
+                    question: "What type of activities do you want to experience on this trip?",
+                    answer: ''
+                },
+                {
+                    questionId: 2,
+                    question: "Which location do you prefer to to explore other than the current location of the trip?",
+                    answer: ''
+                },
+                {
+                    questionId: 3,
+                    question: "What is type of cuisine are you interested in trying for this trip?",
+                    answer: ''
+                },
+                {
+                    questionId: 4,
+                    question: "What is your price level in term of ($-$$$$) sign?",
+                    answer: ''
+                },
+                {
+                    questionId: 5,
+                    question: "Is there anything else you'd like us to know?",
+                    answer: ''
+                }
+            ]
+        };
+        this.handleChange = this.handleChange.bind(this);
+
+       /* AXIOS.create({
+            baseURL: 'http://localhost:4000/trip/survery'});
+
+        AXIOS.post('http://localhost:4000/trip/survey', this.state)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        */
+    }
 
     handleChange(e){
-        let id = e.target.currentQuestion;
+        let id = e.target.questionId;
         let input = e.target.value;
-        this.setState({userAnswer: input});
+        this.setState({answer: input});
         this.saveAnswer(e);
         console.log(id);
         console.log(input);
     }
 
 
-    saveAnswer(){
-       // e.preventDefault();
-       const {currentQuestion} = this.state;
-       const surveyQuestion = {
-          questions: QuestionData[currentQuestion].question,
-          options: QuestionData[currentQuestion].options,
-          answers: QuestionData[currentQuestion].answer 
-        }
-      
-        console.log(JSON.stringify(surveyQuestion));
-        AXIOS.put('http://localhost:4000/question', surveyQuestion)
+    saveAnswer(e){
+        e.preventDefault();
+        
+        AXIOS.put('http://localhost:4000/question', this.state)
         .then(response => {
             console.log(response)
         })
@@ -49,113 +77,43 @@ export class Survey extends Component{
         })
         
        
-    }    
-        
-          loadSurvey = () => {
-            const {currentQuestion} = this.state;
-            this.setState(() => {
-              return{
-                questions: QuestionData[currentQuestion].question,
-                options: QuestionData[currentQuestion].options,
-                answers: QuestionData[currentQuestion].answer
-              }
-            })
-            this.saveAnswer();
-          }
-          
-          componentDidMount() {
-            this.loadSurvey();
-          }
-    
-          nextQuestion = () => {
-            
-            this.setState({
-            
-              currentQuestion: this.state.currentQuestion + 1
-            });
-            
-          }
-          
-          componentDidUpdate(prevProps, prevState){
-            const {currentQuestion} = this.state;
-            if(this.state.currentQuestion !== prevState.currentQuestion){
-              this.setState(() => {
-                return{
-                  questions: QuestionData[currentQuestion].question,
-                  options: QuestionData[currentQuestion].options,
-                  answers: QuestionData[currentQuestion].answer
-                }
-              })
-            }
-          }
-          
-    
-          setAnswer = answer =>{
-            this.setState({
-              userAnswer: answer
-            })
-            console.log(answer)
-          }
-    
-          setFinish = () =>{
-            if(this.state.currentQuestion === QuestionData.length - 1){
-              this.setState({
-                quizEnd: true
-              })
-            }
-          }
+    }
+
     
     render(){
-        const {questions, options, currentQuestion, userAnswer, quizEnd} = this.state;
-       
-        if(quizEnd){
-            return(
-                <div>
-                    <h2>Thanks for taking our Survey!</h2>
-                    <Button variant="primary" href="/Home">
-                        Home
-                    </Button>
-                </div>
-            )
-        }
         return(
-            <div className="App">
-                <h1>SURVEY</h1>
-            <h4>{questions}</h4>
-            <span>Questions  {currentQuestion + 1}  out of {QuestionData.length} </span>
-
-            {options.map(option => (
-                <p key = {option.id}
-                    className={userAnswer===option ? "selected": null} 
-                    onClick={()=> this.setAnswer(option)}
-                > 
-                    {option}
-                </p>
-            ))}
-            
-            {currentQuestion < QuestionData.length - 1 &&
-                <button 
-                    onClick = {this.nextQuestion}
-                >Next</button>
-            }
-
-            {currentQuestion === QuestionData.length - 1 &&
-                <button
-                    onClick={this.setFinish}
-                >Finish</button>
-            }
-        </div>     
-           
+            <Modal
+      {...this.props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter" value='./images/TRIPPIN.png'>
+        <img className="logo"src={require("./images/TRIPPIN.png")} width="50px" height="50px"/>SURVEY
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className='question'>
+            <p>
+                <div>{this.state.questions.map((question, i) => (<div key={question.questionId}>
+                <div>{question.questionId}. {question.question}</div>
+                <div><input type='text' placeholder='Enter answer' value={this.state.questions.answer} onChange={this.handleChange}/></div>
+                </div>))}
+                </div>
+            </p>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='primary' onClick={this.props.onHide}>Done</Button>
+      </Modal.Footer>
+    </Modal>
         );
     }
-        
-   
-        
+
     /* addQuestion(){
         AXIOS.post("http://localhost:4000/survey",{
             this.state.questions}
         })
     } */
 }
-
-export default Survey;
