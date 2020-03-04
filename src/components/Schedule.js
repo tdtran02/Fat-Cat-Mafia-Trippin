fc5cd8f0a609142fcfbd104d2bffb8fbfeef7b03
Dongxuan Li <davidli@dhcp-39-9-123.dhcpint.csulb.edu>
2020-03-04 10:59:35 -0800

    drag and drop testing

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -9,10 +9,11 @@ import {
   Card
 } from "react-bootstrap";
 import AXIOS from "axios";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import "../styles/Recommendation.scss";
+// import "../styles/Recommendation.scss";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 class Schedule extends Component {
   constructor(props) {
@@ -25,8 +26,26 @@ class Schedule extends Component {
       days: [],
       days_elements: [],
       daylist: [],
-      loading: true
+      loading: true,
+      initialData: {
+        column: {
+          id: "column-1",
+          numberIds: ["four", "one", "five", "three", "two"]
+        },
+        numbers: {
+          five: { id: "five", content: "5" },
+          four: { id: "four", content: "4" },
+          one: { id: "one", content: "1" },
+          three: { id: "three", content: "3" },
+          two: { id: "two", content: "2" }
+        }
+      }
     };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result) {
+    // the item was dropped!
   }
   componentDidMount() {
     AXIOS.get("http://localhost:4000/tripinfo/" + this.state.trip_id)
@@ -43,7 +62,68 @@ class Schedule extends Component {
   }
 
   render() {
-    return <div>easkjdhskj</div>;
+    const numbers = this.state.initialData.column.numberIds.map(
+      numberId => this.state.initialData.numbers[numberId]
+    );
+    const list = [];
+    for (let i = 0; i < this.state.trip_locations.length; i++) {
+      list.push(
+        <div style={{ border: "1px solid black", margin: "10px" }} key={i}>
+          {this.state.trip_locations[i].name}
+        </div>
+      );
+    }
+    let id2 = "2";
+    let index2 = 22;
+    let id3 = "3";
+    let index3 = 33;
+    return (
+      <DragDropContext droppableId="1" onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="1">
+          {provided => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              {...provided.droppablePlaceholder}
+            >
+              <Draggable draggableId={id2} index={index2}>
+                {provided => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    1
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId={id3} index={index3}>
+                {provided => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    2
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+        {/* <div column={this.state.column}></div> */}
+      </DragDropContext>
+      //   <Droppable
+      //     droppableId="1"
+      //     style={{ width: "200px", border: "1px solid black" }}
+      //   >
+      //     {provided => (
+      //       <span innerRef={provided.innerRef} {...provided.droppableProps}>
+      //         {list.length != 0 ? list : ""}
+      //       </span>
+      //     )}
+      //   </Droppable>
+    );
   }
 }
 
