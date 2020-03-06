0e723f94eb2544559855d465c8e35b9733ebf41e
Dongxuan Li <davidli@Davids-MacBook-Pro.local>
2020-03-06 00:13:56 -0800

    dayslist

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -6,7 +6,8 @@ import {
   Dropdown,
   Form,
   Button,
-  Card
+  Card,
+  Carousel
 } from "react-bootstrap";
 import AXIOS from "axios";
 // import "slick-carousel/slick/slick.css";
@@ -53,13 +54,20 @@ class Schedule extends Component {
           trip_location_elements: this.createList(this.state.trip_locations)
         });
 
-        console.log(this.state);
+        console.log(res.data.trip);
 
         this.setState({ loading: false });
       })
       .catch(err => {
         console.log(err);
       });
+  }
+
+  getDayList(days) {
+    const elements = [];
+    for (let i = 0; i < days.length; i++) {
+      elements.push(<div></div>);
+    }
   }
   createList(list) {
     // <Draggable draggableId={i.toString()} index={i} key={i}>
@@ -202,8 +210,8 @@ class Schedule extends Component {
                   </Card.Text>
                 </Card.Body> */}
               {/* </Card> */}
-              <div class="card">
-                <div class="row no-gutters">
+              <div className="card">
+                <div className="row no-gutters">
                   {/* <div class="col-auto">
                     <img
                       src={list[i].image_url}
@@ -212,7 +220,7 @@ class Schedule extends Component {
                       style={{ height: "100px", width: "100px" }}
                     />
                   </div> */}
-                  <div class="col">
+                  <div className="col">
                     <div
                       className="card-block px-2"
                       style={{ textOverflow: "ellipsis" }}
@@ -292,32 +300,234 @@ class Schedule extends Component {
     return stars;
   }
 
-  render() {
-    const list = [];
-    for (let i = 0; i < this.state.trip_locations.length; i++) {
-      list.push(
-        <Draggable draggableId={i.toString()} index={i} key={i}>
-          {provided => (
-            <div
-              ref={provided.innerRef}
-              {...provided.draggableProps}
-              {...provided.dragHandleProps}
-              className="draggable"
-              //   style={{ background: "green" }}
-              //   {...provided.draggableplaceholder}
-            >
-              {this.state.trip_locations[i].name}
-            </div>
-          )}
-        </Draggable>
-      );
-    }
+  getUserLocationsDroppable(list) {
     return (
-      <DragDropContext droppableId="1" onDragEnd={this.onDragEnd}>
-        <Droppable
-          droppableId="1"
-          style={{ overflow: "scroll", height: "50px", background: "yellow" }}
+      <Droppable droppableId="userlist" style={{ overflow: "scroll" }}>
+        {provided => (
+          <div
+            className="droppable"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            //   {...provided.droppablePlaceholder}
+          >
+            {list}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    );
+  }
+
+  getDaysDraggableList(list) {
+    const elements = [];
+    // for (let i = 0; i < list.length; i++) {
+    for (let i = 0; i < 5; i++) {
+      const per_day = [];
+      //   for (let j = 0; j < list[i].length; j++) {
+      for (let j = 0; j < 5; j++) {
+        per_day.push(
+          <Draggable draggableId={i.toString()} index={i}>
+            {provided => (
+              <div
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                className="draggable"
+              >
+                {/* {list[i].name} */}
+                haha
+                {provided.placeholder}
+              </div>
+            )}
+          </Draggable>
+        );
+      }
+      elements.push(per_day);
+    }
+    return elements;
+  }
+
+  getDaysDroppable(daysList) {
+    const elements = [];
+    for (let i = 0; i < daysList.length; i++) {
+      {
+        elements.push(
+          <Droppable droppableId={i.toString()}>
+            {provided => (
+              <div
+                className="droppable"
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                //   {...provided.droppablePlaceholder}
+              >
+                {daysList[i]}
+              </div>
+            )}
+          </Droppable>
+        );
+      }
+    }
+    return elements;
+  }
+
+  render() {
+    let settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0
+      //   responsive: [
+      //     {
+      //       breakpoint: 1600,
+      //       settings: {
+      //         slidesToShow: 3,
+      //         slidesToScroll: 3,
+      //         infinite: true,
+      //         dots: true
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 1100,
+      //       settings: {
+      //         slidesToShow: 2,
+      //         slidesToScroll: 2,
+      //         initialSlide: 2
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 800,
+      //       settings: {
+      //         slidesToShow: 1,
+      //         slidesToScroll: 1
+      //       }
+      //     }
+      //   ]
+    };
+
+    let USERLIST;
+    if (this.state.trip_location_elements.length != 0) {
+      USERLIST = this.getUserLocationsDroppable(
+        this.state.trip_location_elements
+      );
+    } else {
+      USERLIST = "";
+    }
+
+    let x = this.getDaysDraggableList(this.state.days);
+    console.log(x);
+    let DAYLIST = this.getDaysDroppable(x);
+    console.log(DAYLIST);
+
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div style={{ backgroundColor: "yellow" }}>
+          <div
+            style={{
+              width: "300px",
+              backgroundColor: "red"
+            }}
+          >
+            {USERLIST}
+          </div>
+
+          <Carousel
+            style={{
+              height: "400px",
+              background: "black",
+              width: "100%",
+              float: "right"
+            }}
+          >
+            <Carousel.Item>
+              <div style={{ width: "300px", margin: "0 auto" }}>
+                <Droppable
+                  droppableId="2"
+                  style={{
+                    overflow: "scroll",
+                    backgroundColor: "white",
+                    color: "white"
+                  }}
+                >
+                  {provided => (
+                    <div
+                      //   className="droppable"
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      //   {...provided.droppablePlaceholder}
+                    >
+                      XD
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </div>
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        {/* <span
+          style={{
+        
+            width: "300px",
+            backgroundColor: "red"
+          }}
         >
+          {USERLIST}
+        </span>
+
+        <span
+          style={{
+            width: "1000px",
+            background: "yellow",
+            margin: "0 auto"
+          }}
+        >
+          <Slider
+            className="placeholderhere"
+            {...settings}
+            style={{
+              width: "80%",
+              margin: "0 auto"
+            }}
+          >
+            {DAYLIST}
+          </Slider>
+        </span> */}
+
+        {/* <Droppable droppableId="2" style={{ overflow: "scroll" }}>
           {provided => (
             <div
               className="droppable"
@@ -325,28 +535,11 @@ class Schedule extends Component {
               {...provided.droppableProps}
               //   {...provided.droppablePlaceholder}
             >
-              {this.state.trip_location_elements.length != 0
-                ? this.state.trip_location_elements
-                : ""}
+             
               {provided.placeholder}
             </div>
           )}
-        </Droppable>
-        <Droppable droppableId="2" style={{ overflow: "scroll" }}>
-          {provided => (
-            <div
-              className="droppable"
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              //   {...provided.droppablePlaceholder}
-            >
-              {/* {this.state.trip_location_elements.length != 0
-                ? this.state.trip_location_elements
-                : ""} */}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
+        </Droppable> */}
       </DragDropContext>
     );
   }
