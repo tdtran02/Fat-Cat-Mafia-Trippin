97e99a2de7bb6c9508228603894d2888bf1b99db
Dongxuan Li <davidli@Davids-MacBook-Pro.local>
2020-04-04 00:57:13 -0700

    fixed the trippin main page

--- src/components/TrippinPage/header.js
+++ src/components/TrippinPage/header.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
   render() {
     return (
-     
       <header>
         <div className="head">
-            <h1>We Design,< br/> You Travel</h1>
+          <h1>
+            We Design,
+            <br /> You Travel
+          </h1>
+          <div>
+            <p>
+              We help you plan and schedule your perfect trip. So you can spend
+              and enjoy your vacation time with friends and family
+            </p>
             <div>
-              <p>I don't know what to write here so i'll just put some random things down just to make it more visual for the eyes.</p>
-              <div><a className="contact" href="#">Learn More</a></div>
+              <a className="contact" href="/home">
+                Let's get started!
+              </a>
             </div>
+          </div>
         </div>
-       
       </header>
-      
     );
   }
 }
