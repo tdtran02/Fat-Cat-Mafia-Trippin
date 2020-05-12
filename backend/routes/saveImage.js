75bbc3ad607830c9e3fe5405f783497db297751e
beibeijoy <xinbeishen@gmail.com>
2020-05-11 22:53:44 -0700

    Update saveImage.js
    Accept jpg file

--- backend/routes/saveImage.js
+++ backend/routes/saveImage.js
@@ -72,7 +72,7 @@ const userStorage = multer.diskStorage({
 
 const profileFileFilter = (req, file, cb) => {
     if(file.mimetype === 'image/jpeg' ||
-    file.mimetype === 'image/png'){
+    file.mimetype === 'image/png'|| file.mimetype === 'image/jpg'){
         cb(null, true);
     }else{
         cb(null, false);
